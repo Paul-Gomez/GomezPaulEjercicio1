c5cf555b1380a676d36dddd434367e5610a547fd
Paul-Gomez <paulgomezzubi@gmail.com>
2024-11-16 14:10:08 +0100

    Genero la matriz con números aleatorios y creo el menú de opciones.

--- src/GomezPaulEjercicio1.java
+++ src/GomezPaulEjercicio1.java
@@ -28,6 +28,31 @@ public class GomezPaulEjercicio1 { //Defino la clase.
                 if (columnas <= 0) System.out.println("El número debe ser entero. Indica el número de columnas:");
             } else {
                 System.out.println("Debes introducir un número entero.");
-                scanner.next();
+                scanner.next(); //Limpio el buffer
             }
         }
+
+        /* Genero la matriz bidimensional con valores aleatorios entre 1 y 9.
+        Utilizo dos bucles for para llenar las filas y columnas con números aleatorios.
+        random.nextInt (9) va del 0 al 8, por ello pongo el +1.
+         */
+         */
+        int[][] matriz = new int[filas][columnas];
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                matriz[i][j] = random.nextInt(9) + 1;
+            }
+        }
+
+        //Uso el loop while hasta que la variable finJuego sea true.
+        boolean finJuego = false;
+        while (!finJuego) {
+            //Menú de opciones.
+            System.out.println("\nSelecciona una opción:");
+            System.out.println("[0] Salir");
+            System.out.println("[1] Mostrar matriz");
+            System.out.println("[2] Poner bomba");
+
+            //Leo la opción del usuario.
+            int opcion = scanner.hasNextInt() ? scanner.nextInt() : -1;
+            scanner.nextLine(); // Limpio el buffer.
